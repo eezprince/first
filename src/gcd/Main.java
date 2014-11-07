3feddfa31209c8e7e94add194a05a0c4647affd4
ZTH <18698840463@163.com>
2014-11-07 11:24:11 +0800

    zth commit

---
+++ src/gcd/Main.java
@@ -0,0 +1,23 @@
+package gcd;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.PrintWriter;
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args)
+	{
+		Scanner cin = new Scanner(new BufferedInputStream(System.in));
+		int T;
+		T = cin.nextInt();
+		for(int i = 1; i <= T; i++)
+		{
+			BigInteger a;
+			a = cin.nextBigInteger();
+		}
+		cin.close();
+	}
+}
